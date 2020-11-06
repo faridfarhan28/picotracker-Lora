25db0a879913c9755bc3e81de570d6ed1bfb607a
MedadRufus <mailto@medadnewman.co.uk>
2020-11-06 22:12:10 +0000

    set default region to North america US915

--- Software/Drivers/BSP/Components/ublox/geofence.c
+++ Software/Drivers/BSP/Components/ublox/geofence.c
@@ -45,8 +45,8 @@
  */
 lora_setting_status_t lora_settings_status = CORRECT; 
 
-LoRaMacRegion_t current_loramac_region = LORAMAC_REGION_AS923;  // Loramac region AS923
-Polygon_t curr_poly_region = AS923925_INDONESIA_polygon; // Indonesia is in this polygon
+LoRaMacRegion_t current_loramac_region = LORAMAC_REGION_US915;  // Loramac region AS923
+Polygon_t curr_poly_region = US902928_NAMERICA_polygon; // Indonesia is in this polygon
 
 tx_permission_t tx_permission = TX_OK;
 
